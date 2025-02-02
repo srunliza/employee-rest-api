26870479172c4990e8134b694b39530f2b0e74a5
Lisa <srunliza@gmail.com>
2025-02-02 20:06:09 +0700

    first commit

---
+++ src/test/java/co/hrd/employeerestapi/EmployeeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.hrd.employeerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
